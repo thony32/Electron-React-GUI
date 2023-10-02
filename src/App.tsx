95ffebdcad62cd70dc67b7d99a0f4a5181bfbca8
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-02 16:26:51 +0300

    installed all needed packages

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className='px-5 py-3 bg-red-400 hover:bg-red-500 duration-300 rounded-lg'>
           count is {count}
         </button>
         <p>
