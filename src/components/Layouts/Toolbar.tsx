cd2f570e2107970f93370c14e13fc5cf6d833bef
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-04 12:00:22 +0300

    TODO: Fix bugs

--- src/components/Layouts/Toolbar.tsx
+++ src/components/Layouts/Toolbar.tsx
@@ -5,8 +5,23 @@ const Toolbar: React.FC = () => {
   
   return (
     <div className="fixed bottom-5 left-[25%] w-1/2 bg-gray-800 p-4 rounded-full">
-      <button className="btn btn-primary btn-xs">
-        Button
+      <button className="btn btn-primary btn-sm">
+        Select all
+      </button>
+      <button className="btn btn-primary btn-sm">
+        Reset Zoom
+      </button>
+      <button className="btn btn-primary btn-sm">
+        Zoom In
+      </button>
+      <button className="btn btn-primary btn-sm">
+        Zoom Out
+      </button>
+      <button className="btn btn-primary btn-sm">
+        Layer up
+      </button>
+      <button className="btn btn-primary btn-sm">
+        Layer Down
       </button>
     </div>
   );
---
+++ src/components/Main/RefCanvas.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RefCanvas: React.FC = () => {
+  return (
+    <div>RefCanvas</div>
+  )
+}
+
+export default RefCanvas
