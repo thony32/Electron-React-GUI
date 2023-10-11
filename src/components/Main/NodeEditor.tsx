9df2577a869c30acf5a4426a7851cd3cad30156c
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-11 11:51:55 +0300

    TODO: Handle video player

--- src/components/Main/NodeEditor.tsx
+++ src/components/Main/NodeEditor.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import ReactFlow, { Controls, Background, MiniMap, applyNodeChanges, OnNodesChange, Node, NodeTypes, NodeResizer, Handle, Position } from "reactflow";
 import "../../App.css";
 import handleDragOver from "../../utils";
+// import Plyr from "plyr";
 
 // Define the initial nodes for the React Flow component
 const initialNodes: Node[] = [
@@ -28,12 +29,14 @@ const ResizableNodeSelected = ({ data, selected }: any) => {
 const nodeTypes: NodeTypes = {
   ResizableNodeSelected,
 };
-// Define the NodeEditor component
-const NodeEditor: React.FC = () => {
+
+// TODO: Handle VIDEO UPLOAD AND PLAY
+//* Define the NodeEditor component
+const NodeEditor: React.FC = () => { 
   // State to store nodes and media items
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
 
-  //* Function to handle drop of media files into React Flow
+  //? Function to handle drop of media files into React Flow
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
@@ -56,7 +59,7 @@ const NodeEditor: React.FC = () => {
         setNodes((prevNodes: any) => [...prevNodes, newNode]);
       } else if (file.type.startsWith("video/")) {
         // Handle video file as a new node
-        const videoUrl = URL.createObjectURL(file);
+        // const videoUrl = URL.createObjectURL(file);
         const newNode = {
           id: `video-node-${Date.now()}`,
           type: "ResizableNodeSelected",
@@ -64,34 +67,32 @@ const NodeEditor: React.FC = () => {
             label: (
               <div>
                 <video controls playsInline className="w-full h-full">
-                  <source src={videoUrl} type={file.type} />
+                  <source src="https://www.youtube.com/watch?v=nLQ-9vfEjUI" type={file.type} />
                 </video>
               </div>
+              
+              // <Plyr />
+              // <Plyr/>
             ),
           },
           position: { x: event.clientX - 100, y: event.clientY - 100 },
         };
 
         // Add the new video node to the nodes state
-        setNodes((prevNodes: any) => [...prevNodes, newNode]);
+        setNodes((prevNodes: any) => [...prevNodes, newNode])
       }
     }
   };
 
-  const onNodesChange: OnNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), [])
+  const onNodesChange: OnNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []);
 
   return (
     <div className="h-full col-span-12">
-      <div className="h-full border-dashed flex flex-col justify-center items-center" onDrop={handleDrop} onDragOver={handleDragOver}>
+      <div className="h-full flex flex-col justify-center items-center" onDrop={handleDrop} onDragOver={handleDragOver}>
         {/* React Flow component */}
         <ReactFlow nodes={nodes} nodeTypes={nodeTypes} onNodesChange={onNodesChange} onConnect={() => {}} fitView snapToGrid={true} snapGrid={[15, 15]}>
-          {/* Flow background */}
           <Background />
-
-          {/* Controls for the Flow */}
           <Controls className="bg-gray-300 " />
-
-          {/* MiniMap for navigation */}
           <MiniMap className="scale-[.65] lg:scale-[.80] 2xl:scale-100" />
         </ReactFlow>
       </div>
