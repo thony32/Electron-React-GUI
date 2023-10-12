130b7b6b5fd4f0aef3ee7cdb7bafefde0648b52d
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-12 11:21:03 +0300

    Oktober 12th

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
-import { Login, Canvas, Registration } from "./pages";
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import React from "react"
+import { Login, Canvas, Registration } from "./pages"
 
 const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
-          <Route path="/register" element={<Registration/>}/>
-          <Route path="/" element={<Canvas/>}/>
+          <Route path="/" element={<Canvas />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
