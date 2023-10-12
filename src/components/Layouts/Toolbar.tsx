7ffe4ff0a1834993fd79401f1a97b8caa8b9b87e
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-12 14:18:23 +0300

    Code reorganization

--- src/components/Layouts/Toolbar.tsx
+++ src/components/Layouts/Toolbar.tsx
@@ -1,25 +1,22 @@
-import React from "react";
-import SelectAllBtn from "../Toolbar/SelectAllBtn";
-import ResetZoomBtn from "../Toolbar/ResetZoomBtn";
-import ZoomInBtn from "../Toolbar/ZoomInBtn";
-import ZoomOutBtn from "../Toolbar/ZoomOutBtn";
-import LayerUpBtn from "../Toolbar/LayerUpBtn";
-import LayerDownBtn from "../Toolbar/LayerDownBtn";
-import { ThemeChanger } from "..";
-
+import React from "react"
+import ResetZoomBtn from "../Toolbar/ResetZoomBtn"
+import ZoomInBtn from "../Toolbar/ZoomInBtn"
+import ZoomOutBtn from "../Toolbar/ZoomOutBtn"
+import LayerUpBtn from "../Toolbar/LayerUpBtn"
+import LayerDownBtn from "../Toolbar/LayerDownBtn"
+import { ThemeChanger } from ".."
 
 const Toolbar: React.FC = () => {
-  
   return (
     <div className="fixed bottom-5 left-[25%] w-1/2 bg-neutral-content p-4 rounded-full flex justify-between z-10">
-      <ThemeChanger/>
-      <ResetZoomBtn/>
-      <ZoomInBtn/>
-      <ZoomOutBtn/>
-      <LayerUpBtn/>
-      <LayerDownBtn/>
+      <ThemeChanger />
+      <ResetZoomBtn />
+      <ZoomInBtn />
+      <ZoomOutBtn />
+      <LayerUpBtn />
+      <LayerDownBtn />
     </div>
-  );
-};
+  )
+}
 
-export default Toolbar;
+export default Toolbar
