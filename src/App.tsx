6721fe571fd6d0af7ca4b9eacd3025fc79b34f4f
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-17 08:48:01 +0300

    TODO: ContextMenu

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { RightClick, Toolbar } from "./components"
 const App: React.FC = () => {
   return (
     <>
-      
       <div className="grid grid-cols-12 h-screen z-20">
         {/* <EditorFilter /> */}
         <Canvas />
