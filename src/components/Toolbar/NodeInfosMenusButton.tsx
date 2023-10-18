ea623b8ea71a8d78b1e7630e2238c5d38a13c368
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-18 16:09:49 +0300

    Rebased

--- src/components/Toolbar/NodeInfosMenusButton.tsx
+++ src/components/Toolbar/NodeInfosMenusButton.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const NodeInfosMenusButton: React.FC = () => {
   return (
-    <div className="drawer drawer-end w-32 ">
+    <div className="drawer drawer-end w-32">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Page content here */}
