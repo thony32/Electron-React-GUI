5d8a718868ec0aae00203007fe7e69f964f23db3
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-18 15:48:24 +0300

    Accordion

--- src/components/Toolbar/NodeListMenusButton.tsx
+++ src/components/Toolbar/NodeListMenusButton.tsx
@@ -1,21 +1,6 @@
-import React, { useState } from "react"
 import { ImageList, VideoList, Others } from ".."
 
 const NodeListMenusButton: React.FC = () => {
-  // TODO: Handle checked state
-  const [selectedInput, setSelectedInput] = useState<string | null>(null)
-
-  const handleInputChange = (id: string) => {
-    if (id === selectedInput) {
-      setSelectedInput(null)
-    } else {
-      setSelectedInput(id)
-    }
-  }
-
-  const isInputChecked = (id: string) => {
-    return id === selectedInput
-  }
 
   return (
     <div className="drawer">
@@ -23,7 +8,7 @@ const NodeListMenusButton: React.FC = () => {
       <div className="drawer-content">
         {/* Page content here */}
         <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">
-          Open drawer
+          Node Outline
         </label>
       </div>
       <div className="drawer-side">
@@ -31,34 +16,13 @@ const NodeListMenusButton: React.FC = () => {
         <ul className="menu p-4 w-96 min-h-full bg-base-200 text-base-content flex flex-col gap-4">
           {/* Sidebar content here */}
           <li>
-            {/* <ImageList /> */}
-            <div className="collapse collapse-arrow bg-base-300">
-              <input type="radio" checked={isInputChecked("input1")} onChange={() => handleInputChange("input1")} />
-              <div className="collapse-title text-xl font-medium bg-red-200 w-80">Your Ref Images</div>
-              <div className="collapse-content w-80">
-                <p>hello</p>
-              </div>
-            </div>
+            <ImageList />
           </li>
           <li>
-            {/* <VideoList /> */}
-            <div className="collapse collapse-arrow bg-base-300">
-              <input type="radio" checked={isInputChecked("input2")} onChange={() => handleInputChange("input2")} />
-              <div className="collapse-title text-xl font-medium w-80">Your Ref Images</div>
-              <div className="collapse-content w-80">
-                <p>hello</p>
-              </div>
-            </div>
+            <VideoList />
           </li>
           <li>
-            {/* <Others /> */}
-            <div className="collapse collapse-arrow bg-base-300">
-              <input type="radio" checked={isInputChecked("input3")} onChange={() => handleInputChange("input3")} />
-              <div className="collapse-title text-xl font-medium w-80">Your Ref Images</div>
-              <div className="collapse-content w-80">
-                <p>hello</p>
-              </div>
-            </div>
+            <Others />
           </li>
         </ul>
       </div>
