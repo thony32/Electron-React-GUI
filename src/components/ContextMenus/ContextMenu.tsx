b83a07aff14ceba98843bc512295fd58d5b69eea
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-10-18 21:39:50 +0300

    FIXED: ContextMenu Position

--- src/components/ContextMenus/ContextMenu.tsx
+++ src/components/ContextMenus/ContextMenu.tsx
@@ -4,10 +4,10 @@ import type { ContextMenuProps } from "../../utils"
 
 const ContextMenu: React.FC<ContextMenuProps> = ({top, left}) => {
 
-  const containerClasses = `absolute top-[${top}px] left-[${left}px] flex flex-col w-24 lg:w-36 bg-base-300 rounded-md z-50`
+  const containerClasses = `absolute flex flex-col w-24 lg:w-36 bg-base-300 rounded-md z-50`
 
   return (
-    <div className={containerClasses}>
+    <div className={containerClasses} style={{top: top, left: left}}>
       {ContextMenuData.map((menu, index) => {
         return (
           <button className="py-1 px-3 text-xs hover:bg-base-200 duration-300 rounded-md" key={index}>
