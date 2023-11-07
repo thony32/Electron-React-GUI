5a52f201e702cf66b4327f357f5df3a190afa7e1
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-07 16:00:56 +0300

    Add Text Feature done

--- src/components/Inputs/InputText.tsx
+++ src/components/Inputs/InputText.tsx
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import { Text } from "../../assets"
 
-const InputText: React.FC = () => {
-  const openModal = () => {
-    const modal = document.getElementById("inputtext");
-    if (modal instanceof HTMLDialogElement) {
-      modal.showModal();
+const InputText: React.FC<{ addTextNode: (text: string) => void }> = ({ addTextNode }) => {
+  const [text, setText] = useState("");
+
+  const handleAddClick = () => {
+    if (text.trim()) {
+      addTextNode(text);
+      // Close the modal here if needed
+      setText(""); // Reset the text field
     }
   };
+  const openModal = () => {
+    const modal = document.getElementById("inputtext")
+    if (modal instanceof HTMLDialogElement) {
+      modal.showModal()
+    }
+  }
   return (
     <>
       <button className="p-2 rounded-md hover:bg-base-200 duration-300 active:scale-95" onClick={openModal}>
@@ -22,6 +31,8 @@ const InputText: React.FC = () => {
               <input
                 type="text"
                 className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-base-content bg-transparent rounded-lg border-1 border-base-content appearance-none focus:outline-none focus:ring-0 peer"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
                 placeholder=" "
               />
               <label className="absolute text-sm text-base-content duration-300 bg-base-100 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
@@ -29,7 +40,7 @@ const InputText: React.FC = () => {
               </label>
             </div>
             <div className="flex justify-end">
-              <button className="py-2 px-5 bg-neutral/75 rounded-md hover:bg-neutral/90 active:scale-95 text-neutral-content font-bold duration-300">Add</button>
+              <button className="py-2 px-5 bg-neutral/75 rounded-md hover:bg-neutral/90 active:scale-95 text-neutral-content font-bold duration-300" onClick={handleAddClick}>Add</button>
             </div>
           </div>
           {/* Button Close */}
--- src/components/Layouts/Toolbar.tsx
+++ src/components/Layouts/Toolbar.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Undo, Redo } from "../../assets"
 import { InputText, Shortcuts } from ".."
 
-const Toolbar: React.FC = () => {
+const Toolbar: React.FC<{ addTextNode: (text: string, position?: { x: number, y: number }) => void }> = ({addTextNode}) => {
   return (
     <div className="fixed bottom-5  max-[500px]:hidden left-[40%] min-[2560px]:left-[50%] xl:w-64 bg-base-300 px-4 py-2 rounded-md flex gap-4 justify-between">
       <button className="p-2 rounded-md hover:bg-base-200 duration-300 active:scale-95">
@@ -11,7 +11,7 @@ const Toolbar: React.FC = () => {
       <button className="p-2 rounded-md hover:bg-base-200 duration-300 active:scale-95">
         <Redo />
       </button>
-      <InputText />
+      <InputText addTextNode={addTextNode} />
       <Shortcuts />
     </div>
   )
