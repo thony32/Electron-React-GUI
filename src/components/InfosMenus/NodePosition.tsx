e6a8e2519d8a1518cd8697813f6decc315c4353b
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-10 11:22:29 +0300

    as any

--- src/components/InfosMenus/NodePosition.tsx
+++ src/components/InfosMenus/NodePosition.tsx
@@ -1,20 +1,19 @@
-
 import React from "react"
 import "../../index.css"
 import { useNodesAndEdgesState } from "../../hooks"
 import { Node } from "reactflow"
 
 const NodePosition: React.FC = () => {
-  const {nodes, setNodes} = useNodesAndEdgesState() as any
+  const { nodes, setNodes } = useNodesAndEdgesState() as any
 
   const handleXPositionChange = (id: string, newXValue: string) => {
     const value = newXValue.trim() === "" ? "0" : newXValue
-    setNodes((prevNodes: any) => prevNodes.map((node: Node) => (node.id === id ? { ...node, position: { ...node.position, x: parseFloat(value) } } : node)))
+    setNodes((prevNodes: Node[]) => prevNodes.map((node: Node) => (node.id === id ? { ...node, position: { ...node.position, x: parseFloat(value) } } : node)))
   }
 
   const handleYPositionChange = (id: string, newYValue: string) => {
     const value = newYValue.trim() === "" ? "0" : newYValue
-    setNodes((prevNodes: any) => prevNodes.map((node: Node) => (node.id === id ? { ...node, position: { ...node.position, y: parseFloat(value) } } : node)))
+    setNodes((prevNodes: Node[]) => prevNodes.map((node: Node) => (node.id === id ? { ...node, position: { ...node.position, y: parseFloat(value) } } : node)))
   }
 
   return (
@@ -27,11 +26,21 @@ const NodePosition: React.FC = () => {
           <div className="flex justify-center gap-4">
             <div className="text-xs flex justify-center items-center gap-4">
               <span>X:</span>
-              <input type="text" className="block py-1 px-0 w-1/3 text-xs bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 peer text-current" value={node.position.x.toFixed(0)} onChange={(e) => handleXPositionChange(node.id, e.target.value)} />
+              <input
+                type="text"
+                className="block py-1 px-0 w-1/3 text-xs bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 peer text-current"
+                value={node.position.x.toFixed(0)}
+                onChange={(e) => handleXPositionChange(node.id, e.target.value)}
+              />
             </div>
             <div className="text-xs flex justify-center items-center gap-4">
               <span>Y:</span>
-              <input type="text" className="block py-1 px-0 w-1/3 text-xs bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 peer text-current" value={node.position.y.toFixed(0)} onChange={(e) => handleYPositionChange(node.id, e.target.value)} />
+              <input
+                type="text"
+                className="block py-1 px-0 w-1/3 text-xs bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 peer text-current"
+                value={node.position.y.toFixed(0)}
+                onChange={(e) => handleYPositionChange(node.id, e.target.value)}
+              />
             </div>
           </div>
         </div>
