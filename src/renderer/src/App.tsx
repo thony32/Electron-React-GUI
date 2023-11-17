9c0f2bd933d6bb12df7a39d5dba947199bc43136
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-17 15:13:58 +0300

    React device detect

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,27 +5,37 @@ import { ReactFlowProvider } from "reactflow"
 import { RecoilRoot } from "recoil"
 import { CanvasContextProvider, NodesAndEdgesContextProvider, ReactFlowContextProvider } from "./contexts"
 import { NodeInfosBar, NodesListBar, ThemeChanger } from "./components"
+import { BrowserView, MobileView } from "react-device-detect"
 
 const App: React.FC = () => {
   return (
-    <div className="hurme">
-      <RecoilRoot>
-        <ReactFlowProvider>
-          <NodesAndEdgesContextProvider>
-            <ReactFlowContextProvider>
-              <CanvasContextProvider>
-                <div>
-                  <ThemeChanger />
-                  <NodeInfosBar />
-                  <FlowChart />
-                  <NodesListBar />
-                </div>
-              </CanvasContextProvider>
-            </ReactFlowContextProvider>
-          </NodesAndEdgesContextProvider>
-        </ReactFlowProvider>
-      </RecoilRoot>
-    </div>
+    <>
+      <BrowserView>
+        <div className="hurme">
+          <RecoilRoot>
+            <ReactFlowProvider>
+              <NodesAndEdgesContextProvider>
+                <ReactFlowContextProvider>
+                  <CanvasContextProvider>
+                    <div>
+                      <ThemeChanger />
+                      <NodeInfosBar />
+                      <FlowChart />
+                      <NodesListBar />
+                    </div>
+                  </CanvasContextProvider>
+                </ReactFlowContextProvider>
+              </NodesAndEdgesContextProvider>
+            </ReactFlowProvider>
+          </RecoilRoot>
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-3xl font-bold text-center">This app is not available on mobile devices</h1>
+        </div>
+      </MobileView>
+    </>
   )
 }
 
