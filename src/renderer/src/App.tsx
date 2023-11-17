58881a711508ba28c3108e9a7bb4501b1f51d128
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-17 15:56:19 +0300

    React device detect

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,35 +8,35 @@ import { NodeInfosBar, NodesListBar, ThemeChanger } from "./components"
 import { BrowserView, MobileView } from "react-device-detect"
 
 const App: React.FC = () => {
-  return (
-    <>
-      <BrowserView>
-        <div className="hurme">
-          <RecoilRoot>
-            <ReactFlowProvider>
-              <NodesAndEdgesContextProvider>
-                <ReactFlowContextProvider>
-                  <CanvasContextProvider>
-                    <div>
-                      <ThemeChanger />
-                      <NodeInfosBar />
-                      <FlowChart />
-                      <NodesListBar />
-                    </div>
-                  </CanvasContextProvider>
-                </ReactFlowContextProvider>
-              </NodesAndEdgesContextProvider>
-            </ReactFlowProvider>
-          </RecoilRoot>
-        </div>
-      </BrowserView>
-      <MobileView>
-        <div className="flex justify-center items-center h-screen">
-          <h1 className="text-3xl font-bold text-center">This app is not available on mobile devices</h1>
-        </div>
-      </MobileView>
-    </>
-  )
+    return (
+        <>
+            <BrowserView>
+                <div className="hurme">
+                    <RecoilRoot>
+                        <ReactFlowProvider>
+                            <NodesAndEdgesContextProvider>
+                                <ReactFlowContextProvider>
+                                    <CanvasContextProvider>
+                                        <div>
+                                            <ThemeChanger />
+                                            <NodeInfosBar />
+                                            <FlowChart />
+                                            <NodesListBar />
+                                        </div>
+                                    </CanvasContextProvider>
+                                </ReactFlowContextProvider>
+                            </NodesAndEdgesContextProvider>
+                        </ReactFlowProvider>
+                    </RecoilRoot>
+                </div>
+            </BrowserView>
+            <MobileView>
+                <div className="flex justify-center items-center h-screen">
+                    <h1 className="text-3xl font-bold text-center">This app is not available on mobile devices</h1>
+                </div>
+            </MobileView>
+        </>
+    )
 }
 
 export default App
